ea46dd233abedf9a2f2b5f0f6d774113e75b9ff7
MohamedYoussef <medyousef95@gmail.com>
2022-04-13 21:36:10 +0200

    toggle led

--- led.py
+++ led.py
@@ -1,20 +1,33 @@
 import RPi.GPIO as GPIO
 import time
 BUTTON_PIN=26
-LED_PIN = 17
+LED_PIN_1 = 17
+LED_PIN_2 = 27
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(LED_PIN_1, GPIO.OUT)
+GPIO.setup(LED_PIN_2, GPIO.OUT)
 GPIO.setup(BUTTON_PIN, GPIO.IN)
 while(1):
     if (GPIO.input(BUTTON_PIN)):
-        GPIO.output(LED_PIN,GPIO.HIGH)
+        GPIO.output(LED_PIN_1,GPIO.HIGH)
         time.sleep(1)
-        GPIO.output(LED_PIN,GPIO.LOW)
+        GPIO.output(LED_PIN_1,GPIO.LOW)
         time.sleep(1)
-        print("button pressed, LED blinks")
+        print("button pressed, LEDs blinks")
     else:
-        GPIO.output(LED_PIN,GPIO.LOW)
-        print("button released, LED Off")
+        GPIO.output(LED_PIN_1,GPIO.LOW)
+        print("button released, LEDs Off")
+        time.sleep(2)
+while(1):
+    if (GPIO.input(BUTTON_PIN)):
+        time.sleep(1)
+        GPIO.output(LED_PIN_2,GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(LED_PIN_2,GPIO.LOW)       
+        print("button pressed, LEDs blinks")
+    else:
+        GPIO.output(LED_PIN_2,GPIO.LOW)
+        print("button released, LEDs Off")
         time.sleep(2)
 
 
