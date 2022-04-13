b01ecd2dca9b3948a4a20f07398ba0ce0f67a691
MohamedYoussef <medyousef95@gmail.com>
2022-04-13 04:46:58 +0200

    LED controlled by button

--- led.py
+++ led.py
@@ -3,12 +3,20 @@ import time
 BUTTON_PIN=26
 LED_PIN = 17
 GPIO.setmode(GPIO.BCM)
-""" state=int(input("1 to start 0 to exit: ")) """
 GPIO.setup(LED_PIN, GPIO.OUT)
 GPIO.setup(BUTTON_PIN, GPIO.IN)
 while(1):
-    print(GPIO.input(BUTTON_PIN))
-    time.sleep(1)
+    if (GPIO.input(BUTTON_PIN)):
+        GPIO.output(LED_PIN,GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(LED_PIN,GPIO.LOW)
+        time.sleep(1)
+        print("button pressed, LED blinks")
+    else:
+        GPIO.output(LED_PIN,GPIO.LOW)
+        print("button released, LED Off")
+
+
 GPIO.cleanup()
 """ if state==1:
     while(1):
