2fabd4f595e0bb3401161a4986862fb5eb38c731
MohamedYoussef <medyousef95@gmail.com>
2022-04-14 00:47:44 +0200

    3 states pin 2

--- led.py
+++ led.py
@@ -1,58 +1,42 @@
 import RPi.GPIO as GPIO
 import time
-BUTTON_PIN=26
-LED_PIN_1 = 17
-LED_PIN_2 = 27
+
+LED_1_PIN = 17
+LED_2_PIN = 27
+BUTTON_PIN = 26
+
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_1_PIN, GPIO.OUT)
+GPIO.setup(LED_2_PIN, GPIO.OUT)
 GPIO.setup(BUTTON_PIN, GPIO.IN)
-GPIO.setup(LED_PIN_1, GPIO.OUT)
-GPIO.setup(LED_PIN_2, GPIO.OUT)
-GPIO.output(LED_PIN_1,GPIO.LOW)
-GPIO.output(LED_PIN_2,GPIO.LOW)
 
-previous_button_state=GPIO.input(BUTTON_PIN)
-led_index=0
-while(1):
+GPIO.output(LED_1_PIN, GPIO.LOW)
+GPIO.output(LED_2_PIN, GPIO.LOW)
+GPIO.output(LED_3_PIN, GPIO.LOW)
+
+previous_button_state = GPIO.input(BUTTON_PIN)
+led_index = 0
+
+while True:
     time.sleep(0.01)
-    button_state=0
-    if button_state !=previous_button_state:
-        previous_button_state=button_state
+    button_state = GPIO.input(BUTTON_PIN)
+    if button_state != previous_button_state:
+        previous_button_state = button_state
         if button_state == GPIO.HIGH:
-            if led_index==0:
-                GPIO.output(LED_PIN_1,GPIO.HIGH)
-                GPIO.output(LED_PIN_2,GPIO.LOW)
-                led_index=1
-                print(led_index)
-            elif led_index==1:
-                GPIO.output(LED_PIN_1,GPIO.LOW)
-                GPIO.output(LED_PIN_2,GPIO.HIGH)
-                led_index=2 
-                print(led_index)       
-            else:
-                GPIO.output(LED_PIN_1,GPIO.HIGH)
-                GPIO.output(LED_PIN_2,GPIO.HIGH)
-                print(led_index)
-                led_index=0
+            if led_index == 0:
+                GPIO.output(LED_1_PIN, GPIO.HIGH)
+                GPIO.output(LED_2_PIN, GPIO.LOW)
                 
-
-
-
-
-
-
+                led_index = 1
+            elif led_index == 1:
+                GPIO.output(LED_1_PIN, GPIO.LOW)
+                GPIO.output(LED_2_PIN, GPIO.HIGH)
+                
+                led_index = 2
+            else:
+                GPIO.output(LED_1_PIN, GPIO.HIGH)
+                GPIO.output(LED_2_PIN, GPIO.HIGH)
+                
+                led_index = 0
 
 GPIO.cleanup()
-
-""" if state==1:
-    while(1):
-        GPIO.output(LED_PIN,GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(LED_PIN,GPIO.LOW)
-        time.sleep(1)
-elif state==0:
-    GPIO.output(LED_PIN,GPIO.LOW)
-else:
-    print("wrong number")
-    GPIO.cleanup()
-    exit """
-
