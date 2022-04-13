12567ea1f185bac84d8d155b7f358a0b8e219834
MohamedYoussef <medyousef95@gmail.com>
2022-04-13 03:33:00 +0200

    led python with user control

--- led.py
+++ led.py
@@ -2,9 +2,15 @@ import RPi.GPIO as GPIO
 import time
 LED_PIN = 17
 GPIO.setmode(GPIO.BCM)
-while(1):
-    GPIO.setup(LED_PIN, GPIO.OUT)
-    GPIO.output(LED_PIN,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(LED_PIN,GPIO.LOW)
+state=int(input("1 to start 0 to exit"))
+if state==1:
+    while(1):
+        GPIO.setup(LED_PIN, GPIO.OUT)
+        GPIO.output(LED_PIN,GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(LED_PIN,GPIO.LOW)
+elif state==0:
+    exit
+else:
+    print("wrong number")
 GPIO.cleanup()
