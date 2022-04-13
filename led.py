4747a5213d8e206386788983afd8fb24d595512a
medyousef <medyousef95@gmail.com>
2022-04-13 04:27:12 +0200

    led code finish

--- led.py
+++ led.py
@@ -2,15 +2,18 @@ import RPi.GPIO as GPIO
 import time
 LED_PIN = 17
 GPIO.setmode(GPIO.BCM)
-state=int(input("1 to start 0 to exit"))
+state=int(input("1 to start 0 to exit: "))
+GPIO.setup(LED_PIN, GPIO.OUT)
 if state==1:
     while(1):
-        GPIO.setup(LED_PIN, GPIO.OUT)
         GPIO.output(LED_PIN,GPIO.HIGH)
         time.sleep(1)
         GPIO.output(LED_PIN,GPIO.LOW)
+        time.sleep(1)
 elif state==0:
-    exit
+    GPIO.output(LED_PIN,GPIO.LOW)
 else:
     print("wrong number")
-GPIO.cleanup()
+    GPIO.cleanup()
+    exit
+
