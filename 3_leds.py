0fc018790acc7182b6916168f58e4186509d048a
MohamedYoussef <medyousef95@gmail.com>
2022-05-11 16:14:48 +0200

    3 leds with one button with lists

--- 3_leds.py
+++ 3_leds.py
@@ -30,7 +30,7 @@ while True:
                 led_index=2
                 print ("LED ON: ",LED_LIST[0], "LED OFF:",LED_LIST[1:2] )
             else:    
-                LED_state(LED_LIST[1],LED_LIST[0,2])
+                LED_state(LED_LIST[1],(LED_LIST[0],LED_LIST[2]))
                 print ("LED ON: ",LED_LIST[1], "LED OFF:",(LED_LIST[0],LED_LIST[2]))
                 led_index=0
 
